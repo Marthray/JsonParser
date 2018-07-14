54d756e32d2ff35eb19c3f7a40f6cc6ecfaa0f97
Brian <brianjesusmoreno@gmail.com>
2018-07-13 20:28:53 -0400

    Parseo de JSON casi listo, sin probar

--- assets/js/jsonParser.js
+++ assets/js/jsonParser.js
@@ -1,7 +1,17 @@
-$(function() {
+$(function() { //Esta función hace cosas cuando el html carga
 	//obteniendo valores de las cookies
+	var zip;
+	var propType;
+	var bathrooms;
+	var bedrooms;
+	var minP;
+	var maxP;
 
-    var zip = Cookies.get('zip');
+	cargarCookie();
+});
+
+function cargarCookie(){
+	var zip = Cookies.get('zip');
 	var propType = Cookies.get('type');
 	var bathrooms = Cookies.get('bathrooms');
 	var bedrooms = Cookies.get('bedrooms');
@@ -16,7 +26,7 @@ $(function() {
     Cookies.remove('minPrice');
     Cookies.remove('maxPrice');
 
-    $('#result').append(`
+    /*$('#result').append(`
     	<div>
     		`+zip+`
     	</div>
@@ -35,5 +45,28 @@ $(function() {
     	<div>
     		`+maxP+`
     	</div>
-    	`)
-});
+    	`)*/
+}
+
+function parseJson(){
+	$.ajax({
+	    url: "https://rets.io/api/v2/test/listings?access_token=7f8afaacb6f6f5cd2c80f3ee8f9bb103&PostalCode[eq]="+zip+"&PropertyType[in]="+propType+"&BathroomsTotalInteger[eq]="+bathrooms+"&BedroomsTotal[eq]="+bedrooms+"&OriginalListPrice[gte]="+minP+"&OriginalListPrice[lte]="+maxP+"&limit=100",
+	    //force to handle it as text
+	    dataType: "text",
+	    success: function(data) {
+
+	        //data downloaded so we call parseJSON function 
+	        //and pass downloaded data
+	        var listings = $.parseJSON(data);
+	        //now json variable contains data in json format
+	        //let's display a few items
+	        for (var i=0;i<listings.length;++i)
+	        {
+	            $('#result').append(`
+	            	<div>
+	            	<p>Zip: `+listings+`</p>
+	            	</div>`);
+	        }
+	    }
+	});
+}
