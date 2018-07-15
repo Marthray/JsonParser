66c1399cc4b9e0f71a741238632a302c294aa919
Brian <brianjesusmoreno@gmail.com>
2018-07-14 22:03:41 -0400

    Paginación casi lista

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -15,34 +15,16 @@ $('#send').click(function(e){
 	var bedrooms = $('#bed').val();
 	var minP = $('#min-price').val();
 	var maxP = $('#max-price').val();
-	
-	Cookies.set('zip', zip);
-	Cookies.set('type', propType);
-	Cookies.set('bathrooms', bathrooms);
-	Cookies.set('bedrooms', bedrooms);
-	Cookies.set('minPrice', minP);
-	Cookies.set('maxPrice', maxP);
 
-	window.location.href = 'probando.html';
+	if((!isNaN(zip)) && (!isNaN(bathrooms)) && (!isNaN(bedrooms)) && (!isNaN(minP)) && (!isNaN(maxP))) {
+		Cookies.set('zip', zip);
+		Cookies.set('type', propType);
+		Cookies.set('bathrooms', bathrooms);
+		Cookies.set('bedrooms', bedrooms);
+		Cookies.set('minPrice', minP);
+		Cookies.set('maxPrice', maxP);
+		window.location.href = 'probando.html';
+	} else {
+		alert ("Please, all of the values must be numeric excepting \"Property Type\"");
+	}
 });
-	
-/*//Función que cambia el color de fondo y guarda el valor en la cookie
-function cambiacolor(bgcolor){ 							//Recibe el color de fondo 'bgcolor'
-	jQuery('body').css('background-color',bgcolor);		//Cambia el color de fondo de la web a 'bgcolor'	
-	Cookies.set('colorCookie',bgcolor);					//Crea una cookie llamada 'colorCookie' con el color elegido 'bgcolor'
-}
-
-//Al cargar la página se pone de color de fondo el valor que hay en la cookie 
-jQuery('body').css('background-color',Cookies.get('colorCookie'));
-
-//Al clicar sobre la imagen obtenemos el color del atributo 'data-color' y llamamos la función 'cambiacolor' con el color seleccionado
-jQuery('.thumbnail').click(function(e){
-	e.preventDefault();
-	colorseleccionado = jQuery(this).data('color');
-	cambiacolor(colorseleccionado);		
-});
-
-//Al clicar sobre el botón de eliminar cookies, borramos la cookie	
-jQuery("#eliminaCookie").click(function(){
-	Cookies.remove('colorCookie');
-});*/
--- assets/js/jsonParser.js
+++ assets/js/jsonParser.js
@@ -9,6 +9,7 @@ $(function() { //Esta función hace cosas cuando el html carga
 
 	cargarCookie();
 	parseJson();
+	//pagination();
 });
 
 function cargarCookie(){
@@ -53,17 +54,23 @@ function parseJson(){
 
 	var jsonRetsLy = "https://rets.io/api/v2/test/listings?access_token=7f8afaacb6f6f5cd2c80f3ee8f9bb103&PostalCode[eq]="+zip+"&PropertyType[in]="+propType+"&BathroomsTotalInteger[lte]="+bathrooms+"&BedroomsTotal[lte]="+bedrooms+"&OriginalListPrice[gte]="+minP+"&OriginalListPrice[lte]="+maxP+"&limit=100";
     $.getJSON(jsonRetsLy, function (listings) {
-    
-	    for (var i=0;i<listings.bundle.length;++i)
-	    {
-	        $('#result').append(`
-	        	<div style="border: 1px solid;">
+        for (var i=0;i<listings.bundle.length;++i){
+        	$('#result').append(`
+        	<li><div class="listing" style="border: 1px solid;">
 	        	<p>Zip: `+listings.bundle[i].PostalCode+`</p>
 	        	<p>Bathrooms: `+listings.bundle[i].BathroomsTotalInteger+`</p>
 	        	<p>Bedrooms: `+listings.bundle[i].BedroomsTotal+`</p>
 	        	<p>Property Type: `+listings.bundle[i].PropertyType+`</p>
 	        	<p>Pricing: `+listings.bundle[i].OriginalListPrice+`</p>
-	        	</div>`);
-	    }
+        	</div></li>`);
+		}
+	});
+}
+
+function pagination(){
+	var listingList = new List('result', {
+	  valueNames: ['listing'],
+	  page: 5,
+	  pagination: true
 	});
 }
