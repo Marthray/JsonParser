87d26cb94d7fcb722e5720e69d866bc96c059ad0
Brian <brianjesusmoreno@gmail.com>
2018-07-28 11:29:31 -0400

    Completo todo 2

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -23,8 +23,8 @@ $('#send').click(function(e){
 	var propType = $('#prop-type').val();
 	var bathrooms = init($('#bath').val());
 	var bedrooms = init($('#bed').val());
-	var minP = init($('#min-price').val());
-	var maxP = init($('#max-price').val());
+	var minP = $('#min-price').val();
+	var maxP = $('#max-price').val();
 	var rent = $('#rent').val();
 
 	if( rent =="For Rent"){
@@ -32,21 +32,23 @@ $('#send').click(function(e){
 	} else {rent = "false";}
 
 	if(zip != ""){
-		if((parseInt(zip) % 1 == 0) && (parseInt(bathrooms) % 1 == 0) && (parseInt(bedrooms) % 1 == 0) && (parseInt(minP) % 1 == 0) && (parseInt(maxP) % 1 == 0)) {
-			if(parseInt(maxP) >= parseInt(minP)){
-				console.log(accounting.formatNumber(parseInt(zip)));
-				Cookies.set('zip', zip);
-				Cookies.set('type', propType);
-				Cookies.set('bathrooms', bathrooms);
-				Cookies.set('rent', rent);
-				Cookies.set('bedrooms', bedrooms);
-				Cookies.set('minPrice', minP);
-				Cookies.set('maxPrice', maxP);
-				//window.location.href = 'https://soulierproperties.com/list-properties/';
-				window.location.href = 'probando.html';
-			} else {
-				alert ("Minimum price MUST be lower than maximum price");
-			}
+		if((parseInt(zip) % 1 == 0) && (parseInt(bathrooms) % 1 == 0) && (parseInt(bedrooms) % 1 == 0) && (parseInt(init(minP)) % 1 == 0) && (parseInt(init(maxP)) % 1 == 0)) {
+			if((minP!="" || maxP=="")&&(minP=="" || maxP!="")){
+				if(parseInt(maxP) >= parseInt(minP)){
+					console.log(accounting.formatNumber(parseInt(zip)));
+					Cookies.set('zip', zip);
+					Cookies.set('type', propType);
+					Cookies.set('bathrooms', bathrooms);
+					Cookies.set('rent', rent);
+					Cookies.set('bedrooms', bedrooms);
+					Cookies.set('minPrice', minP);
+					Cookies.set('maxPrice', maxP);
+					//window.location.href = 'https://soulierproperties.com/list-properties/';
+					window.location.href = 'probando.html';
+				} else {
+					alert ("Minimum price MUST be lower than maximum price");
+				}
+			} else {alert("If you input minimum or maximum price, it MUST has its counterpart")}
 		} else {
 			alert ("Please, all of the values must be numeric excepting \"Property Type\"");
 		}
